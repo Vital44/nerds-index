cb5b2b11b2178f3c13223f0c3b97402f94e11b6e
Shooker <VitalSmirn@yandex.ru>
2017-10-12 00:12:16 +0300

    добил модалку

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 var link = document.querySelector('.link');
 var modal = document.querySelector(".modal");
+var	name = modal.querySelector("[name=name]")
 var closeModal = modal.querySelector('.close');
 
 link.addEventListener("click", function(e){
 	e.preventDefault();
 	modal.classList.toggle("open");
+	nam.focus();
 });
 
 closeModal.addEventListener('click', function(){
